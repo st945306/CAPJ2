3e7a2470da7bb3e701e8cc3c89f72ba1fb8d9eac
monghsun <kavin23923@gmail.com>
2015-12-28 19:50:08 +0800

    change to TA version

--- PC.v
+++ PC.v
@@ -1,35 +1,40 @@
 module PC
 (
-  clk_i,
-  rst_i,
-  start_i,
-  HD_i,
-  pc_i,
-  pc_o
+	clk_i,
+    rst_i,
+	start_i,
+	HD_i,
+	pcEnable_i,
+	pc_i,
+	pc_o
 );
 
-// Ports
-input               clk_i, rst_i, start_i, HD_i;
-input   [31:0]      pc_i;
-output  reg [31:0]  pc_o;
+// Interface
+input				   clk_i;
+input				   rst_i;
+input				   start_i;
+input				   HD_i,
+input                  pcEnable_i;
+input	 [31:0]		   pc_i;
+output	 [31:0]	  	   pc_o;
 
-// Wires & Registers
+// Signals
+reg		[31:0]		pc_o;
 
 always@(posedge clk_i or negedge rst_i) begin
-  if(~rst_i) begin
+    if(~rst_i) begin
         pc_o <= 32'b0;
-  end
-  else if(start_i)begin
-    if(HD_i) begin
-      pc_o <= pc_i-4;
     end
     else begin
-      pc_o <= pc_i;
+    	if(HD_i) begin
+    	end
+    	else if(start_i)	begin
+    		if( pcEnable_i )
+    			pc_o <= pc_i;
+    	end
+    	else
+    		pc_o <= 32'b0;
     end
-  end
-  else begin
-      pc_o <= 32'b0;
-  end
 end
 
 endmodule
